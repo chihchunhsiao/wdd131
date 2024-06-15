0d111ca745aa08747220f1b303ff398e29aeb09c
chihchunhsiao <wohlstand1004@gmail.com>
2024-06-15 20:44:05 +0800

    w1 project revise

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,14 +4,14 @@ const currentyear = document.querySelector("#currentyear");
 
 const today = new Date();
 
-currentyear.innerHTML = `&copy;<span class="highlight">${today.getFullYear()}</span> 🐳 Chih Chun Hsiao 🐳`;
+currentyear.innerHTML = `&copy;<span class="highlight">${today.getFullYear()}</span> 🐳 Chih Chun Hsiao 🐳 Taiwan`;
 
 // second <p>
 
 // Get the last modified date
 const lastModified = new Date(document.lastModified);
 
-lastModified.innerHTML = `Last Modification: <span class="highlight">${lastModified.getDate()}/${lastModified.getMonth()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()}</span>; 
+lastModified.innerHTML = `Last Modification: <span class="highlight">${lastModified.getDate()}/${lastModified.getMonth()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()}</span>`; 
 
 
 
