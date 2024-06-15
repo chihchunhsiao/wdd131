f92d48d66072258918d1f7bf58dfef02c9379cf4
chihchunhsiao <wohlstand1004@gmail.com>
2024-06-15 14:31:27 +0800

    w1 project revise

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,14 @@
 // Get the last modified date and time
-const lastModified = document.lastModified;
-console.log(`Last modification: ${lastModified}`);
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.lastModified("#lastModified");
+
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+lastModified.innerHTML = `Last modification: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "lastModified"
+	}
+).format(today)}</span>`; 
+
